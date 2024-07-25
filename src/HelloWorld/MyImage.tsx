80ea96276bf85b3add4ee361851c73451ef33631
YutingYantina <112956127+YutingYantina@users.noreply.github.com>
2024-07-25 13:25:41 -0400

    Update MyImage.tsx

--- src/HelloWorld/MyImage.tsx
+++ src/HelloWorld/MyImage.tsx
@@ -9,7 +9,6 @@ export const MyImage: React.FC<MyImageProps> = ({ src }) => {
   const frame = useCurrentFrame();
   const { fps, durationInFrames } = useVideoConfig();
 
-  // 使用 spring 函数创建一个动画
   const scale = spring({
     frame,
     fps,
@@ -19,7 +18,6 @@ export const MyImage: React.FC<MyImageProps> = ({ src }) => {
     },
   });
 
-  // 使用 interpolate 函数将动画值映射到图像的透明度
   const opacity = interpolate(frame, [0, durationInFrames / 5], [0, 1], {
     extrapolateLeft: 'clamp',
     extrapolateRight: 'clamp',
